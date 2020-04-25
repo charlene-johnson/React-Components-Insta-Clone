ef2291f7884c24ccfb40da189bff369c92dbaf77
charlene-johnson <charlene.j8234@gmail.com>
2020-04-24 20:57:18 -0400

    Finishing up Commentzsection, need to style the page

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,21 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
+import { counter } from "@fortawesome/fontawesome-svg-core";
 // import data 
-
+import Dummy from "../../dummy-data"
 const PostsPage = () => {
   // set up state for your data
+ const [state, setState] = useState(Dummy)
+
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {state.map(item=>{
+        return <Post post={item}/>
+      })}
     </div>
   );
 };
